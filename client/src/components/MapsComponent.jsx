c45ac7934cfa23112d4d396067e58945f81d312b
Marilena Alexe <marilena.alexe@gmail.com>
2017-07-23 13:10:29 +0100

    Add MapsComponent.jsx file under components folder

---
+++ client/src/components/MapsComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class MapsComponent extends React.Component {
+
+  render() {
+    return(
+      <div>
+        <h2>Here there will be some maps!</h2>
+      </div>
+      )
+  }
+
+}
+
+export default MapsComponent;
--- client/src/container/ContainerBox.jsx
+++ client/src/container/ContainerBox.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import MapsComponent from '../components/MapsComponent.jsx'
 
 class ContainerBox extends React.Component {
 
   render() {
     return(
       <div className="whisky">
-      <p>{this.props.title}</p>
+      <h1>{this.props.title}</h1>
+      <MapsComponent/>
       </div>
       )
   }
