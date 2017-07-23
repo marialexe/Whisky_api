7e8871e5781dea3516a542cd8dbd7d063918cd61
Marilena Alexe <marilena.alexe@gmail.com>
2017-07-23 13:20:33 +0100

    Add Description.jsx file under components folder

---
+++ client/src/components/Description.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Description extends React.Component {
+
+  render() {
+    return(
+      <div>
+        <h2>Description section</h2>
+      </div>
+      )
+  }
+}
+
+export default Description;
--- client/src/container/ContainerBox.jsx
+++ client/src/container/ContainerBox.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import MapsComponent from '../components/MapsComponent.jsx'
+import Description from '../components/Description.jsx'
 
 class ContainerBox extends React.Component {
 
@@ -8,6 +9,7 @@ class ContainerBox extends React.Component {
       <div className="whisky">
       <h1>{this.props.title}</h1>
       <MapsComponent/>
+      <Description/>
       </div>
       )
   }
