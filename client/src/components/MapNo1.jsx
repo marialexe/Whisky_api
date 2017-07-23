666d93c31da01037cfdcc5f39198e651bdd2e598
Marilena Alexe <marilena.alexe@gmail.com>
2017-07-23 14:49:32 +0100

    Add MapNo1.jsx (renders a map) as a subcomponent of MapsComponent.jsx

---
+++ client/src/components/MapNo1.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import GoogleMap from "react-google-map"
+// import GoogleMapLoader from "react-google-maps-loader"
+class MapNo1 extends React.Component {
+
+  componentDidMount() {
+    const map1 = new google.maps.Map(this.refs.map, {
+      center: {lat: 57.194490, lng: -3.823812},
+      // 56.872348, -4.323766
+      // scrollwheel: false,
+      zoom: 6
+    });
+  }
+
+  render() {
+    return(
+      // <div ref="map" style={mapStyle} ref="map">
+      <div ref="map" style={{width: 300, height: 300, border: '1px solid black'}} >
+        <h3>Map No.1!</h3>
+      </div>
+      )
+  }
+
+}
+
+// MapNo1.propTypes ={
+//   intital.position: React.PropTypes.object.isRequired;
+// };
+
+
+// PiggyBank.propTypes = {
+//   title: PropTypes.string
+//   // title: PropTypes.string.isRequired
+// }
+
+export default MapNo1;
--- client/src/components/MapsComponent.jsx
+++ client/src/components/MapsComponent.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 
+import MapNo1 from './MapNo1.jsx'
+
 class MapsComponent extends React.Component {
 
+  
   render() {
     return(
+      // <div ref="map" style={mapStyle} ref="map">
       <div>
         <h2>Here there will be some maps!</h2>
+        <MapNo1/>
       </div>
       )
   }
 
 }
+
 
 export default MapsComponent;
